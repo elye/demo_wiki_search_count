417948f447d027c662038390db0ab7c73984a657
Elisha Lye <elisha.lye@rea-group.com>
2020-05-05 20:46:06 +1000

    Update to android x

--- app/src/main/java/com/elyeproj/wikisearchcount/MainActivity.kt
+++ app/src/main/java/com/elyeproj/wikisearchcount/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.elyeproj.wikisearchcount
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import io.reactivex.android.schedulers.AndroidSchedulers
